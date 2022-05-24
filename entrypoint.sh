d7ea5359bd322c99692ae037033edef0645e294a
vpanov <vpanov@citc.ru>
2022-05-24 17:21:58 +0300

    rework file

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,6 @@ if [ ! -f /etc/samba/smb.conf ]; then
         --adminpass="${SAMBA_DC_ADMIN_PASSWD}" \
         --server-role=dc \
         --realm="${SAMBA_DC_REALM}" \
-        --dns-backend="${SAMBA_DC_DNS_BACKEND}" \
         --use-rfc2307 \
         --function-level=2008_R2 \
         --option="allow dns updates = disabled"
